b250c29bbbeba1b45eec27042a4d7bd6e0021b4b
vreddy <vreddy@us.ibm.com>
2021-03-24 23:15:42 -0400

    implementing test framework and sample login test

--- keycloak-extensions/src/test/java/org/alvearie/keycloak/KeycloakContainerTest.java
+++ keycloak-extensions/src/test/java/org/alvearie/keycloak/KeycloakContainerTest.java
@@ -11,9 +11,14 @@ import static org.junit.Assert.assertThat;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
 
 import org.alvearie.keycloak.config.KeycloakConfigurator;
 import org.alvearie.keycloak.config.util.KeycloakConfig;
+import org.jsmart.zerocode.core.httpclient.BasicHttpClient;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,6 +31,10 @@ import dasniko.testcontainers.keycloak.KeycloakContainer;
 public class KeycloakContainerTest {
     private static final String MASTER_REALM = "master";
     private static final String CLIENT_ID = "admin-cli";
+    private static final String USERNAME = "a";
+    private static final String PASSWORD = "a";
+    private static final String KC_CLIENT = "test";
+    private static final String TOKEN_ENDPOINT = "/auth/realms/test/protocol/openid-connect/token";
 
     private static KeycloakContainer keycloak;
     static {
@@ -48,13 +57,14 @@ public class KeycloakContainerTest {
                 .clientId(CLIENT_ID)
                 .build();
         KeycloakConfigurator configurator = new KeycloakConfigurator(adminClient);
-        KeycloakConfig config = new KeycloakConfig("keycloak-config.json");
+       KeycloakConfig config = new KeycloakConfig("keycloak-config.json");
         configurator.initializeRealm("test", config.getPropertyGroup("test"));
     }
 
     @AfterClass
     public static void end() {
         // no cleanup to enable re-use of the container
+    	keycloak.close();
     }
 
     @Test
@@ -70,5 +80,29 @@ public class KeycloakContainerTest {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         assertEquals(connection.getResponseCode(), 200);
+    }
+    
+    @Test
+    public void passwordGrantLoginTest() {
+    	System.out.println("Host is: " + keycloak.getHost());
+    	String tokenURL = "http://localhost:" + keycloak.getHttpPort() + TOKEN_ENDPOINT;
+    	String jsonbody = "{\"grant_type\":\"password\",\"username\":\"" + USERNAME + "\",\"password\":\"" + PASSWORD + "\",\"client_id\":\""
+                + KC_CLIENT + "\"}";
+    	BasicHttpClient bh = new BasicHttpClient();
+        Map<String, Object> headers = new HashMap<String, Object>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, String> tokenDetails = new HashMap<>();
+        Response r;
+        try {
+            r = bh.execute(
+            		tokenURL,
+                    "POST", headers, null, jsonbody);
+
+            System.out.println(r.getEntity().toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+           
+        }
     }
 }
