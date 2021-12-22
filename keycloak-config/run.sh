e38102d13f05924363cb0536b1d9574c266a89e7
Lee Surprenant <lmsurpre@us.ibm.com>
2021-12-22 17:29:26 -0500

    replace keycloak-config Dockerfile with fabric8's docker maven plugin
    In this experiment, I replaced the multi-stage docker build (docker on
    top) with a maven configuration that builds the docker file (maven on
    top).
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- keycloak-config/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-###############################################################################
-# (C) Copyright IBM Corp. 2021
-#
-# SPDX-License-Identifier: Apache-2.0
-###############################################################################
-
-# Execute the cli jar
-java -cp 'jars/*' org.alvearie.keycloak.config.Main $@
